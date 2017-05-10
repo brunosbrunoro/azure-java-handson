98415c7862e99069ef527c6f3f5a0942b7245ec3
Bruno Scrok Brunoro <bruno.sbrunoro@gmail.com>
2017-05-09 22:30:22 -0300

    Commit init

---
+++ src/br/com/bruno/handsonazure/AppResources.java
@@ -0,0 +1,22 @@
+package br.com.bruno.handsonazure;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by bruno on 09/05/17.
+ */
+@Path("/app")
+public class AppResources {
+
+    @GET
+    public String somar(@QueryParam("valor1") String valor1,@QueryParam("valor2") String valor2) {
+        Double val1 = Double.parseDouble(valor1);
+        Double val2 = Double.parseDouble(valor2);
+
+        Double resultado = val1+val2;
+
+        return "Somar Resultado - "+resultado;
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bruno
+  Date: 09/05/17
+  Time: 20:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Hands On Azure</title>
+  </head>
+  <body>
+    Hands On Azure CWB
+  </body>
+</html>
